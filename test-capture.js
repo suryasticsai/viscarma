24c0a9bcb593540ba2f8fb9f30b307a775511ade
Sai Varakala <suryasuprince@gmail.com>
2026-06-29 16:31:03 +0530

    Delete test-capture.js

--- test-capture.js
+++
@@ -1,6 +0,0 @@
-const { captureAppState } = require('./lib/capture');
-
-(async () => {
-  const result = await captureAppState('https://suryasticsai.github.io/alkembicneo/');
-  console.log('Console errors captured:', JSON.stringify(result.consoleErrors, null, 2));
-})();
